e0facb95975fb3ad831d3e7b123b255bc3b19595
Tom Dickman <twdickman@gmail.com>
2020-07-11 13:13:36 +1000

    add skills content and smooth scrolling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,27 +2,37 @@ import React from 'react'
 
 import styles from './Banner.css'
 
-const Banner = (props) => {
+const Subtitle = (props) => {
+  if (props.text) {
+    return (
+      <p className={`${styles.subtitle} ${styles.bannerText}`}>
+        <span>{props.text}</span>
+      </p>
+    )
+  }
+}
 
-  const subtitle = () => {
+class Banner extends React.Component {
 
-    if (props.subtitle) {
-      return(
-        <p className={`${styles.subtitle} ${styles.bannerText}`}>
-          <span>{props.subtitle}</span>
-        </p>
-      )
-    }
+  constructor(props) {
+    super(props)
+    this.handleScroll = this.handleScroll.bind(this)
   }
 
-  return(
-    <div className={styles.banner}>
-      <h1 className={styles.bannerText}>
-        <span className={styles.title}>{props.title}</span>
-      </h1>
-      {subtitle()}
-    </div>
-  )
+  handleScroll(event) {
+
+  }
+
+  render() {
+    return(
+      <div className={styles.banner}>
+        <h1 className={styles.bannerText} onScroll={this.handleScroll}>
+          <span className={styles.title}>{this.props.title}</span>
+        </h1>
+        <Subtitle text={this.props.subtitle} />
+      </div>
+    )
+  }
 }
 
 export default Banner
--- src/components/Home.js
+++ src/components/Home.js
@@ -15,25 +15,30 @@ function Home() {
         title='Tom Dickman'
         subtitle='Web Developer'
       />
-      <Container id='about' className={styles.aboutMe} >
+      <Container id='about' className={styles.about}>
         <h1>Hi there, I'm Tom</h1>
         <p>I am a <strong>full-stack developer</strong>, with a passion for contributing to open source projects and developing new features and APIs.</p>
         <p>You can view some of my contributions to open source projects, and plugins I've built over on my <a href='https://github.com/tomdickman'><strong>Github <FontAwesomeIcon icon={['fab', 'github']} /> page</strong></a>.</p>
         <p>If you want to see what features I have contributed to <strong>Moodle</strong>, the largest open-source learning platform on the web, you can look for my name in the <a href='https://moodle.org/dev/contributions.php'>developer credits</a></p>
       </Container>
-      <Spacer height='10vh' />
-      <Container id='skills'>
-        <FontAwesomeIcon icon={['fab', 'html5']} />
-        <FontAwesomeIcon icon={['fab', 'css3']} />
-        <FontAwesomeIcon icon={['fab', 'js']} />
-        <FontAwesomeIcon icon={['fab', 'php']} />
-        <FontAwesomeIcon icon={['fab', 'react']} />
-        <FontAwesomeIcon icon={['fab', 'aws']} />
+      <Spacer height='20vh' />
+      <Container id='skills' className={styles.skills}>
+        <h1>Things I'm skilled with...</h1>
+        <div className={styles.skillsIcons}>
+          <div className={styles.skillsIcon}><FontAwesomeIcon icon={['fab', 'html5']}  size='4x' title='HTML5' /></div>
+          <div className={styles.skillsIcon}><FontAwesomeIcon icon={['fab', 'css3']} size='4x' title='CSS' /></div>
+          <div className={styles.skillsIcon}><FontAwesomeIcon icon={['fab', 'js']} size='4x' title='Javascript' /></div>
+          <div className={styles.skillsIcon}><FontAwesomeIcon icon={['fab', 'php']} size='4x' title='PHP' /></div>
+          <div className={styles.skillsIcon}><FontAwesomeIcon icon={['fab', 'react']} size='4x' title='ReactJS' /></div>
+          <div className={styles.skillsIcon}><FontAwesomeIcon icon={['fab', 'aws']} size='4x' title='Amazon Web Services' /></div>
+        </div>
       </Container>
-      <Container id='education'>
+      <Spacer height='20vh' />
+      <Container id='education' className={styles.education}>
 
       </Container>
-      <Container id='experience'>
+      <Spacer height='20vh' />
+      <Container id='experience' className={styles.experience}>
 
       </Container>
     </div>
